caa1c4f6b0d902e75dd0a469b055f44d77577ed4
Taha Maqsood Ali <81623971+tahamaqsood@users.noreply.github.com>
2022-03-28 14:04:34 +0500

    Add files via upload

---
+++ Assignements/Assignment01/Car.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication16.Models
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+    }
+}
